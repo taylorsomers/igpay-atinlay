896f54c72d30b7979279b7eea849f63f0e706983
Taylor Somers <tsomers@yandex.com>
2020-06-10 17:21:32 -0400

    Add sufficient business and UI logic to demonstrate first spec, i.e. 'The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels.'

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 // Business Logic:
 
-
-
+function translate(input) {
+  const output = input;
+  return output;
+}
 // End Business Logic
 
 // UI Logic
 
 $(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+    const toTranslate = $("#user-input").val();
+    const pigLatin = translate(toTranslate);
 
+    $("#results").text(pigLatin);
+  });
 });
 
 // End UI Logic
